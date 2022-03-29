6354fb15f3414bec5183182eab1dbc4aa41f0534
GonzalezNacho <nacho.ldm63@gmail.com>
2022-03-29 13:48:01 -0300

    checkeo de datos

--- js/script.js
+++ js/script.js
@@ -3,17 +3,29 @@
 function imprimirResultadoAreaCirculo() {
     let radioCirculoEnTexto = document.getElementById("radioSolicitadoCirculo");
     let radioCirculo = parseFloat(radioCirculoEnTexto.value);
-    let resultado = parseInt(Math.PI * Math.pow(radioCirculo,2));
-    imprimirEnElHTML(resultado, "formularioCirculo");    
+    if (radioCirculo > 0 && radioCirculo <= 100) {
+        imprimirEnElHTML(parseInt(Math.PI * Math.pow(radioCirculo,2)), "formularioCirculo"); 
+    } else {
+        imprimirError(radioCirculo,"radio");
+    }
 }
+
 
 function imprimirResultadoAreaTriangulo() {
     let alturaTrianguloEnTexto = document.getElementById("alturaSolicitadoTriangulo");
     let baseTrianguloEnTexto = document.getElementById("baseSolicitadoTriangulo");
     let baseTriangulo = parseFloat(baseTrianguloEnTexto.value);
     let alturaTriangulo = parseFloat(alturaTrianguloEnTexto.value);
-    let resultado = parseInt((baseTriangulo * alturaTriangulo)/2);
-    imprimirEnElHTML(resultado, "formularioTriangulo");   
+    if (alturaTriangulo > 0 && baseTriangulo > 0 && alturaTriangulo <= 100 && baseTriangulo <= 100) {
+        let resultado = parseInt((baseTriangulo * alturaTriangulo)/2);
+        imprimirEnElHTML(resultado, "formularioTriangulo");  
+    } else {
+        if (baseTriangulo > 0 && baseTriangulo <= 100) {
+            imprimirError(alturaTriangulo,"altura");
+        } else {
+            imprimirError(baseTriangulo,"base");
+        } 
+    }     
 }
 
 
@@ -25,9 +37,12 @@ function imprimirEnElHTML (resultado, elementoAnterior) {
     currentDiv.appendChild(newP); 
 }
 
-/*function comprobarNumero (numero) {
-    if (numero > 0 && numero =< 100) {
-
+function imprimirError (numero,numeroAOperarEnString) {
+    let error = "No se puede operar el numero:";
+    if (numero < 0) {
+        error += `\n -El numero de ${numeroAOperarEnString} es negativo`;
+    } else {
+        error += "\n -Solamente se pueden operar numeros desde 1 a 100";
     }
-
-}*/
+    alert(error);
+}
